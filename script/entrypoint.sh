d86cfc1826effc64cdb1cabdecc9ecf0f544f734
Puckel_ <contact@puckel.Fr>
2015-11-13 16:06:57 +0100

    Rename DB_LOOP to TRY_LOOP

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CMD="airflow"
-DB_LOOPS="10"
+TRY_LOOP="10"
 MYSQL_HOST="mysql"
 MYSQL_PORT="3306"
 RABBITMQ_HOST="rabbitmq"
@@ -16,17 +16,13 @@ if [ "$@" = "webserver" ] || [ "$@" = "worker" ] || [ "$@" = "scheduler" ] || [ 
   j=0
   while ! curl -sI -u $RABBITMQ_CREDS http://$RABBITMQ_HOST:15672/api/whoami |grep '200 OK'; do
     j=`expr $j + 1`
-    if [ $j -ge $DB_LOOPS ]; then
+    if [ $j -ge $TRY_LOOP ]; then
       echo "$(date) - $RABBITMQ_HOST still not reachable, giving up"
       exit 1
     fi
-    echo "$(date) - waiting for RabbitMQ... $j/$DB_LOOPS"
-    sleep 2
+    echo "$(date) - waiting for RabbitMQ... $j/$TRY_LOOP"
+    sleep 5
   done
-fi
-
-if [ "$@" = "flower" ]; then
-  sleep 10
 fi
 
 # wait for DB
@@ -34,12 +30,12 @@ if [ "$@" = "webserver" ] || [ "$@" = "worker" ] || [ "$@" = "scheduler" ] ; the
   i=0
   while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
     i=`expr $i + 1`
-    if [ $i -ge $DB_LOOPS ]; then
+    if [ $i -ge $TRY_LOOP ]; then
       echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
       exit 1
     fi
-    echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}... $i/$DB_LOOPS"
-    sleep 1
+    echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}... $i/$TRY_LOOP"
+    sleep 5
   done
   if [ "$@" = "webserver" ]; then
     echo "Initialize database..."
