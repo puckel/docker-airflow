e00f5534600b1173b0c09eca614c6defb645f180
Joe Nap <joe.napolitano@wework.com>
2018-06-08 12:22:10 -0400

    Read from env var

--- script/git-sync.sh
+++ script/git-sync.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-set -exuo pipefail
+set -euo pipefail
 
-git-sync.py --repo git@github.com:WeConnect/data-workflows.git --dest /usr/local/airflow/repo --branch pull-test --run-once
+if [[ ! -f ${HOME}/.ssh/id_rsa ]]; then
+  printf "%b\n" "$AIRFLOW_GITHUB_RSA" > ${HOME}/.ssh/id_rsa
+  chmod 600 ${HOME}/.ssh/id_rsa
+fi
 
-ln -sf ${AIRFLOW__CORE__AIRFLOW_HOME}/repo/src/projects/example_project/dags ${AIRFLOW__CORE__AIRFLOW_HOME}/dags/
+git-sync.py \
+  --repo git@github.com:WeConnect/data-workflows.git \
+  --dest /usr/local/airflow/repo \
+  --branch pull-test \
+  --run-once
+
+if [[ ! -L ${HOME}/dags/dags ]]; then
+  ln -sf \
+    ${AIRFLOW__CORE__AIRFLOW_HOME}/repo/src/projects/example_project/dags \
+    ${AIRFLOW__CORE__AIRFLOW_HOME}/dags/
+fi
