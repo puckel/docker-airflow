2beb9b5848b865d0ffe4792bd3dbefeb2b5b0043
Joe Nap <joe.napolitano@wework.com>
2018-06-08 09:50:12 -0400

    use new repo name

--- script/git-sync.sh
+++ script/git-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -exuo pipefail
 
-git-sync.py --repo git@github.com:WeConnect/data-airflow.git --dest /usr/local/airflow/repo --branch pull-test --run-once
+git-sync.py --repo git@github.com:WeConnect/data-workflows.git --dest /usr/local/airflow/repo --branch pull-test --run-once
 
 ln -sf ${AIRFLOW__CORE__AIRFLOW_HOME}/repo/src/projects/example_project/dags ${AIRFLOW__CORE__AIRFLOW_HOME}/dags/
