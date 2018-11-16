44fdd365429d12a00d0c7f7286ea780629df7a45
Julien Pierret <julien.pierret@appfolio.com>
2018-11-16 11:11:27 -0800

    JP - Since we have an uninstall script, don't need the uninstall function in the install script.

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -183,36 +183,8 @@ install_airflow () {
   echo "Airflow is now up and running on: http://localhost:8080/"
 }
 
-##############################################################
-# Function to remove airflow from kubernetes cluster
-##############################################################
-uninstall_airflow() {
-
-  # Remove port forwarding
-  PORT_FORWARD_PROCESS=`ps -ef | grep "kubectl port-forward airflow" | grep -v "grep" | awk '{ print $2 }'`
-  kill ${PORT_FORWARD_PROCESS}
-
-  # Uninstall airflow via helm
-  helm delete --purge airflow
-  echo "Waiting 60 seconds for services to shut down"
-  sleep 60
-
-  # Remove pods that refuse to go away
-  export AIRFLOW_TO_PURGE=`kubectl get pods | grep airflow | cut -f1 -d' '`
-  for i in "${AIRFLOW_TO_PURGE[@]}"
-  do
-    echo "Purging: ${i}"
-    kubectl delete pods $i --grace-period=0 --force
-  done
-  
-  kubectl delete clusterrolebinding airflow 
-  kubectl delete serviceaccount airflow --namespace=${NAMESPACE}
-}
-
-
 setup
 install_airflow
-#uninstall_airflow
 
 ##############################################################
 # Extras
