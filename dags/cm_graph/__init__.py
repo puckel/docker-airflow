a233ec4254b876fb72f8e4d7d44e403061f8c14a
Felix Yuan <felix@classdojo.com>
2019-12-20 02:07:55 +0000

    Create view stuff

--- dags/cm_graph/__init__.py
+++ dags/cm_graph/__init__.py
@@ -1,18 +1,9 @@
-from .create_views import (
-    create_cache_views, mentor_lifecycle,
-    parent_lifecycle, school_leader_lifecycle,
-    teacher_lifecycle
-)
+from .create_views import create_tasks as cv_create_tasks
 
 
-def create_view_tasks(dag):
+def create_tasks(dag):
+    create_view_tasks = cv_create_tasks(dag)
     d = {
-        'cache_teacher': create_cache_views.create_cache_teacher_view_task(dag),
-        'cache_parent': create_cache_views.create_cache_parent_view_task(dag),
-        'cache_schoolleader_mentor': create_cache_views.create_schoolleader_mentor_view_task(dag),
-        'teacher_lifecycle': teacher_lifecycle.create_teacher_lifecycle_view_task(dag),
-        'parent_lifecycle': parent_lifecycle.create_parent_lifecycle_view_task(dag),
-        'schoolleader_lifecycle': school_leader_lifecycle.create_schoolleader_lifecycle_view_task(dag),
-        'mentor_lifecycle': mentor_lifecycle.create_mentor_lifecycle_view_task(dag)
+        'create_view_tasks': create_view_tasks,
     }
     return d
