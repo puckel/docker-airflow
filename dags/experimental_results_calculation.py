5e8efacf0ca7b0e3c893dbde1725780b932b8ac6
Felix Yuan <felix@classdojo.com>
2020-07-01 03:01:54 +0000

    Add a bunch of intermediate result calculations

--- dags/experimental_results_calculation.py
+++ dags/experimental_results_calculation.py
@@ -4,6 +4,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 from datetime import datetime, timedelta
+from dateutil import parser
 
 import pathlib
 import os
@@ -14,8 +15,8 @@ EXPERIMENT_INTERMEDIATE_RESULTS_TABLE = 'ab_platform.experiment_results_intermed
 RESULTS_METADATA_TABLE = 'ab_platform.results_run'
 
 
-def get_active_experiment_and_population_map(conn_id, ts, **kwargs):
-    pg_hook = PostgresHook(conn_id)
+def get_active_experiment_and_population_map(analytics_conn_id, ts, **kwargs):
+    pg_hook = PostgresHook(analytics_conn_id)
     query = '''
     SELECT
         a.experiment_id,
@@ -29,35 +30,35 @@ def get_active_experiment_and_population_map(conn_id, ts, **kwargs):
     for experiment_id, population_kind, table_name in records:
         experiment_id_to_table_mapping[experiment_id] = {
             'population_table_name': table_name,
+            'population_schema_name':  'ab_platform',
             'population_type': population_kind,
         }
     return experiment_id_to_table_mapping
 
 
-def create_intermediate_results_table(conn_id, ts, **kwargs):
-    pg_hook = PostgresHook(conn_id)
+def create_intermediate_results_table(frontend_conn_id, ts, **kwargs):
+    pg_hook = PostgresHook(frontend_conn_id)
     query = '''
     create table if not exists %s
     (
-        experiment_id varchar(36) not null distkey,
+        experiment_id varchar(36) not null,
         variant varchar(128) not null,
         metric_name varchar(128) not null,
         metric_type varchar(128) not null,
         segment varchar(128) not null,
         day timestamp not null,
-        numerator integer encode az64,
-        denominator integer encode az64,
+        numerator integer,
+        denominator integer,
         mean double precision,
-        standard_deviation double precision
-    )
-    diststyle key
-    compound sortkey(day, experiment_id);
+        standard_deviation double precision,
+        primary key(experiment_id, variant, metric_name, metric_type, segment, day)
+    );
     ''' % EXPERIMENT_INTERMEDIATE_RESULTS_TABLE
     pg_hook.run(query)
 
 
-def create_results_run_table(conn_id, ts, **kwargs):
-    pg_hook = PostgresHook(conn_id)
+def create_results_run_table(analytics_conn_id, ts, **kwargs):
+    pg_hook = PostgresHook(analytics_conn_id)
     query = '''
     CREATE TABLE IF NOT EXISTS %s (
         run_id VARCHAR(36) ENCODE ZSTD distkey,
@@ -70,8 +71,16 @@ def create_results_run_table(conn_id, ts, **kwargs):
     pg_hook.run(query)
 
 
-def calculate_intermediate_results(conn_id, ts, **kwargs):
+def calculate_intermediate_results(analytics_conn_id, ts, **kwargs):
     task_instance = kwargs['task_instance']
+    pg_hook = PostgresHook(analytics_conn_id)
+    dt = parser.parse(ts)
+    # Get the last days worth of stuff
+    # Use this instead of the provided 'ds' so we can do some date operations
+    yesterday = dt.date() - timedelta(days=1)
+    print(yesterday)
+    task_instance.xcom_push(key='most_recent_date', value=yesterday)
+
     experiment_to_population_map = task_instance.xcom_pull(
         task_ids='get_active_experiment_and_population_map'
     )
@@ -90,7 +99,97 @@ def calculate_intermediate_results(conn_id, ts, **kwargs):
                 s = f.read()
                 population_templates[population_type][metric_name] = s
 
-    print(population_templates)
+    all_records = []
+    for _, population_metadata in experiment_to_population_map.items():
+        template_map = population_templates.get(
+            population_metadata['population_type'], {})
+        for metric_name, template in template_map.items():
+            s = template % {
+                'metric_name': metric_name,
+                'table_name': '%s.%s' % (population_metadata['population_schema_name'],
+                                         population_metadata['population_table_name']),
+                'ds': yesterday.isoformat(),
+            }
+            records = pg_hook.get_records(s)
+            all_records += records
+
+    return all_records
+
+
+def insert_intermediate_records(frontend_conn_id, ts, **kwargs):
+    task_instance = kwargs['task_instance']
+    pg_hook = PostgresHook(frontend_conn_id)
+    records = task_instance.xcom_pull(
+        task_ids='calculate_intermediate_results'
+    )
+    # We have to worry about double inserts if it runs on the same day
+    # So we delete records from the table with the same experiment_id, metric_name, and date
+
+    for experiment_id, variant, metric_name, metric_type, \
+            segment, day, numerator, denominator, mean, \
+            standard_deviation in records:
+
+        query = '''
+            begin;
+            DELETE FROM %(table_name)s
+            WHERE
+                experiment_id = '%(experiment_id)s' and
+                variant = '%(variant)s' and
+                metric_name = '%(metric_name)s' and
+                metric_type = '%(metric_type)s' and
+                segment = '%(segment)s' and
+                day = '%(day)s';
+
+            INSERT INTO %(table_name)s
+            (experiment_id, variant, metric_name, metric_type, segment, day, numerator, denominator, mean, standard_deviation) VALUES
+            ('%(experiment_id)s', '%(variant)s', '%(metric_name)s', '%(metric_type)s', '%(segment)s', '%(day)s', %(numerator)d, %(denominator)d, %(mean)f, %(standard_deviation)f);
+            commit;
+            ''' % {
+            'table_name': EXPERIMENT_INTERMEDIATE_RESULTS_TABLE,
+            'experiment_id': experiment_id,
+            'variant':  variant,
+            'metric_name': metric_name,
+            'metric_type': metric_type,
+            'segment': segment,
+            'day': day.date().isoformat(),
+            'numerator': numerator,
+            'denominator': denominator,
+            'mean': mean,
+            'standard_deviation': standard_deviation,
+        }
+        pg_hook.run(query)
+
+
+def calculate_results(frontend_conn_id, ts, **kwargs):
+    task_instance = kwargs['task_instance']
+    pg_hook = PostgresHook(frontend_conn_id)
+    most_recent_date = task_instance.xcom_pull(
+        key='most_recent_date'
+    )
+
+    query = '''
+    SELECT * FROM %s WHERE day = '%s'::TIMESTAMP
+    ''' % (EXPERIMENT_INTERMEDIATE_RESULTS_TABLE, most_recent_date.isoformat())
+
+    records = pg_hook.get_records(query)
+
+    grouped_records = {}
+    for experiment_id, variant, metric_name, metric_type, \
+            segment, day, numerator, denominator, mean, \
+            standard_deviation in records:
+
+        variants = grouped_records.get(
+            (experiment_id, metric_name, metric_type), {})
+        variants[variant] = {
+            'segment': segment,
+            'day': day,
+            'numerator': numerator,
+            'denominator': denominator,
+            'mean': mean,
+            'standard_deviation': standard_deviation
+        }
+
+    print(grouped_records)
 
 
 # Default settings applied to all tasks
@@ -112,7 +211,8 @@ with DAG('experimental_results_calculation',
          ) as dag:
 
     default_task_kwargs = {
-        'conn_id': 'analytics_redshift'
+        'analytics_conn_id': 'analytics_redshift',
+        'frontend_conn_id': 'ab_platform_frontend',
     }
 
     start_task = DummyOperator(
@@ -147,9 +247,23 @@ with DAG('experimental_results_calculation',
         provide_context=True
     )
 
+    insert_intermediate_records_task = PythonOperator(
+        task_id='insert_intermediate_results',
+        python_callable=insert_intermediate_records,
+        op_kwargs=default_task_kwargs,
+        provide_context=True
+    )
+
+    caluclate_results_task = PythonOperator(
+        task_id='caculate_results',
+        python_callable=calculate_results,
+        op_kwargs=default_task_kwargs,
+        provide_context=True,
+    )
+
     start_task >> [get_active_experiment_and_population_map_task,
                    create_intermediate_results_table_task,
                    create_results_run_table_task]
 
     [get_active_experiment_and_population_map_task,
-        create_intermediate_results_table_task] >> calculate_intermediate_results_task
+        create_intermediate_results_table_task] >> calculate_intermediate_results_task >> insert_intermediate_records_task >> caluclate_results_task
