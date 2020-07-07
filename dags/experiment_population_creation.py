d9249fdd6766fca49cce6871b2f6f53cea505300
Felix Yuan <felix@classdojo.com>
2020-07-07 20:45:46 +0000

    Add external task sensor for audience and split into two dags

---
+++ dags/experiment_population_creation.py
@@ -0,0 +1,167 @@
+from airflow import DAG
+from airflow.hooks import PostgresHook
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+
+from datetime import datetime, timedelta
+
+import uuid
+
+CONTROL_PANEL_TABLE = 'ab_platform.experiment_control_panel'
+POPULATION_METADATA_TABLE = 'ab_platform.population_run'
+POPULATION_MAPPING_TABLE = 'ab_platform.experiment_to_population_map'
+
+
+def _create_population_run_metadata_table(conn_id):
+    pg_hook = PostgresHook(conn_id)
+    query = '''
+    CREATE TABLE IF NOT EXISTS {} (
+        run_id VARCHAR(36) ENCODE ZSTD DISTKEY,
+        status VARCHAR(128) ENCODE ZSTD,
+        createdat TIMESTAMP DEFAULT SYSDATE
+    )
+    COMPOUND SORTKEY(createdat);
+    '''.format(POPULATION_METADATA_TABLE)
+    pg_hook.run(query)
+
+
+def success_callback(ctx):
+    conn_id = 'analytics_redshift'
+    pg_hook = PostgresHook(conn_id)
+    run_uuid = uuid.uuid4()
+
+    # Create metadata table if it doesn't exist
+    _create_population_run_metadata_table(conn_id)
+
+    query = '''
+    INSERT INTO {} (run_id, status) VALUES ('{}', 'success')
+    '''.format(POPULATION_METADATA_TABLE, run_uuid)
+    pg_hook.run(query)
+
+
+def failure_callback(ctx):
+    conn_id = 'analytics_redshift'
+    pg_hook = PostgresHook(conn_id)
+    run_uuid = uuid.uuid4()
+
+    # Create metadata table if it doesn't exist
+    _create_population_run_metadata_table(conn_id)
+
+    query = '''
+    INSERT INTO {} (run_id, status) VALUES ('{}', 'failure')
+    '''.format(POPULATION_METADATA_TABLE, run_uuid)
+    pg_hook.run(query)
+
+
+def create_mapping_table(conn_id, ts, **kwargs):
+    pg_hook = PostgresHook(conn_id)
+    query = '''
+    CREATE TABLE IF NOT EXISTS {} (
+        experiment_id VARCHAR(36) ENCODE ZSTD DISTKEY,
+        table_name VARCHAR(256) ENCODE ZSTD
+    );
+    '''.format(POPULATION_MAPPING_TABLE)
+    pg_hook.run(query)
+
+
+def get_manually_mapped_tables(conn_id, ts, **kwargs):
+    pg_hook = PostgresHook(conn_id)
+    query = '''
+    SELECT
+        experiment_id,
+        manual_population_override
+    FROM {}
+    WHERE
+        archived = false and population_basis = 'Manual' and
+        manual_population_override is not NULL
+    '''.format(CONTROL_PANEL_TABLE)
+
+    records = pg_hook.get_records(query)
+    d = dict(records)
+    return d
+
+
+def write_mappings(conn_id, ts, **kwargs):
+    task_instance = kwargs['task_instance']
+    pg_hook = PostgresHook(conn_id)
+    manually_mapped_tables = task_instance.xcom_pull(
+        task_ids='get_manually_mapped_tables')
+
+    for experiment_id, table_name in manually_mapped_tables.items():
+        query = '''
+        begin;
+        DELETE FROM %(mapping_table)s
+        WHERE experiment_id='%(experiment_id)s';
+        INSERT INTO %(mapping_table)s (experiment_id, table_name)
+        VALUES ('%(experiment_id)s', '%(table_name)s');
+        commit;
+        ''' % {
+            'mapping_table': POPULATION_MAPPING_TABLE,
+            'experiment_id': experiment_id,
+            'table_name': table_name,
+        }
+        pg_hook.run(query)
+
+
+event_task_sensor = ExternalTaskSensor(
+    task_id='event_task_sensor',
+    external_dag_id='experimental_event_ingest',
+    external_task_id='end',
+    check_existence=True
+)
+
+# Default settings applied to all tasks
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+# Note this is a "dummy" DAG for now.
+with DAG('experimental_population_creation',
+         start_date=datetime(2020, 6, 10),
+         max_active_runs=1,
+         catchup=False,
+         schedule_interval=timedelta(minutes=10),
+         default_args=default_args,
+         on_success_callback=success_callback,
+         on_failure_callback=failure_callback,
+         ) as dag:
+
+    # Tasks for population
+    start_task = DummyOperator(
+        task_id='start'
+    )
+
+    create_mapping_table_task = PythonOperator(
+        task_id='create_mapping_table',
+        python_callable=create_mapping_table,
+        op_kwargs={'conn_id': 'analytics_redshift'},
+        provide_context=True
+    )
+
+    generate_automatic_population_task = DummyOperator(
+        task_id='generate_automatic_population'
+    )
+
+    get_manually_mapped_tables_task = PythonOperator(
+        task_id='get_manually_mapped_tables',
+        python_callable=get_manually_mapped_tables,
+        op_kwargs={'conn_id': 'analytics_redshift'},
+        provide_context=True
+    )
+
+    write_mappings_task = PythonOperator(
+        task_id='write_mappings',
+        python_callable=write_mappings,
+        op_kwargs={'conn_id': 'analytics_redshift'},
+        provide_context=True
+    )
+
+    # DAG for the population ingestion
+    event_task_sensor >> start_task >> create_mapping_table_task >> [
+        generate_automatic_population_task, get_manually_mapped_tables_task] >> write_mappings_task
--- dags/experimental_event_ingest.py
+++ dags/experimental_event_ingest.py
@@ -11,18 +11,12 @@ import uuid
 
 CONTROL_PANEL_TABLE = 'ab_platform.experiment_control_panel'
 EXPERIMENTAL_METADATA_TABLE = 'ab_platform.ingestion_run'
-AUDIENCE_METADATA_TABLE = 'ab_platform.audience_run'
-AUDIENCE_MAPPING_TABLE = 'ab_platform.experiment_to_population_map'
 
 
-def generate_run_uuid(**kwargs):
-    return uuid.uuid4()
-
-
-def create_run_metadata_table(conn_id, ts, **kwargs):
+def _create_run_metadata_table(conn_id):
     pg_hook = PostgresHook(conn_id)
     query = '''
-    CREATE TABLE IF NOT EXISTS %s (
+    CREATE TABLE IF NOT EXISTS {} (
         run_id VARCHAR(36) ENCODE ZSTD DISTKEY,
         start_ts TIMESTAMP,
         end_ts TIMESTAMP,
@@ -31,8 +25,33 @@ def create_run_metadata_table(conn_id, ts, **kwargs):
     )
     COMPOUND SORTKEY(end_ts, start_ts)
     ;
-    ''' % (EXPERIMENTAL_METADATA_TABLE,)
+    '''.format(EXPERIMENTAL_METADATA_TABLE,)
     pg_hook.run(query)
+
+
+def _callback(state, ctx):
+    task_instance = ctx['task_instance']
+    conn_id = 'analytics_redshift'
+    pg_hook = PostgresHook(conn_id)
+    run_uuid = uuid.uuid4()
+    start_ts = task_instance.xcom_pull(key='start_ts')
+    end_ts = task_instance.xcom_pull(key='end_ts')
+    run_uuid = uuid.uuid4()
+    _create_run_metadata_table(conn_id)
+
+    query = '''
+    INSERT INTO %s (run_id, start_ts, end_ts, event_count, status)
+    VALUES('%s', '%s', '%s', 0, '%s')
+    ''' % (EXPERIMENTAL_METADATA_TABLE, run_uuid, start_ts, end_ts, state)
+    pg_hook.run(query)
+
+
+def success_callback(ctx):
+    _callback('success', ctx)
+
+
+def failure_callback(ctx):
+    _callback('failure', ctx)
 
 
 def get_active_experiment_ids(conn_id, ts, **kwargs):
@@ -115,95 +134,6 @@ def generate_intermediate_events(conn_id, ts, **kwargs):
     print(query)
 
 
-def tag_run(conn_id, ts, **kwargs):
-    task_instance = kwargs['task_instance']
-    pg_hook = PostgresHook(conn_id)
-    start_ts = task_instance.xcom_pull(key='start_ts')
-    end_ts = task_instance.xcom_pull(key='end_ts')
-    run_uuid = task_instance.xcom_pull(task_ids='generate_run_uuid')
-
-    query = '''
-    INSERT INTO %s (run_id, start_ts, end_ts, event_count, status)
-    VALUES('%s', '%s', '%s', 0, 'success')
-    ''' % (EXPERIMENTAL_METADATA_TABLE, run_uuid, start_ts, end_ts)
-    pg_hook.run(query)
-
-
-# Audience run
-def create_audience_run_metadata_table(conn_id, ts, **kwargs):
-    pg_hook = PostgresHook(conn_id)
-    query = '''
-    CREATE TABLE IF NOT EXISTS %s (
-        run_id VARCHAR(36) ENCODE ZSTD DISTKEY,
-        status VARCHAR(128) ENCODE ZSTD,
-        createdat TIMESTAMP DEFAULT SYSDATE
-    )
-    COMPOUND SORTKEY(createdat);
-    ''' % AUDIENCE_METADATA_TABLE
-    pg_hook.run(query)
-
-
-def create_mapping_table(conn_id, ts, **kwargs):
-    pg_hook = PostgresHook(conn_id)
-    query = '''
-    CREATE TABLE IF NOT EXISTS %s (
-        experiment_id VARCHAR(36) ENCODE ZSTD DISTKEY,
-        table_name VARCHAR(256) ENCODE ZSTD
-    );
-    ''' % AUDIENCE_MAPPING_TABLE
-    pg_hook.run(query)
-
-
-def get_manually_mapped_tables(conn_id, ts, **kwargs):
-    pg_hook = PostgresHook(conn_id)
-    query = '''
-    SELECT
-        experiment_id,
-        manual_population_override
-    FROM %s
-    WHERE
-        archived = false and population_basis = 'Manual' and
-        manual_population_override is not NULL
-    ''' % CONTROL_PANEL_TABLE
-
-    records = pg_hook.get_records(query)
-    d = dict(records)
-    return d
-
-
-def write_mappings(conn_id, ts, **kwargs):
-    task_instance = kwargs['task_instance']
-    pg_hook = PostgresHook(conn_id)
-    manually_mapped_tables = task_instance.xcom_pull(
-        task_ids='get_manually_mapped_tables')
-
-    for experiment_id, table_name in manually_mapped_tables.items():
-        query = '''
-        begin;
-        DELETE FROM %(mapping_table)s
-        WHERE experiment_id='%(experiment_id)s';
-        INSERT INTO %(mapping_table)s (experiment_id, table_name)
-        VALUES ('%(experiment_id)s', '%(table_name)s');
-        commit;
-        ''' % {
-            'mapping_table': AUDIENCE_MAPPING_TABLE,
-            'experiment_id': experiment_id,
-            'table_name': table_name,
-        }
-        pg_hook.run(query)
-
-
-def tag_population_run(conn_id, ts, **kwargs):
-    task_instance = kwargs['task_instance']
-    pg_hook = PostgresHook(conn_id)
-    run_uuid = task_instance.xcom_pull(task_ids='generate_run_uuid')
-
-    query = '''
-    INSERT INTO %s (run_id, status) VALUES ('%s', 'success')
-    ''' % (AUDIENCE_METADATA_TABLE, run_uuid)
-    pg_hook.run(query)
-
-
 # Default settings applied to all tasks
 default_args = {
     'owner': 'airflow',
@@ -221,23 +151,12 @@ with DAG('experimental_event_ingest',
          catchup=False,
          schedule_interval=timedelta(minutes=10),
          default_args=default_args,
+         on_success_callback=success_callback,
+         on_failure_callback=failure_callback
          ) as dag:
 
     start_task = DummyOperator(
         task_id='start'
-    )
-
-    generate_run_uuid_task = PythonOperator(
-        task_id='generate_run_uuid',
-        python_callable=generate_run_uuid,
-        provide_context=True
-    )
-
-    create_run_metadata_task = PythonOperator(
-        task_id='create_run_metadata_table',
-        python_callable=create_run_metadata_table,
-        op_kwargs={'conn_id': 'analytics_redshift'},
-        provide_context=True,
     )
 
     get_active_experiment_task = PythonOperator(
@@ -269,84 +188,18 @@ with DAG('experimental_event_ingest',
         task_id='populate_experiment_events'
     )
 
-    tag_run_task = PythonOperator(
-        task_id='tag_run',
-        python_callable=tag_run,
-        op_kwargs={'conn_id': 'analytics_redshift'},
-        provide_context=True
-    )
     cleanup_intermediate_tables_task = DummyOperator(
         task_id='cleanup_intermediate_tables'
     )
 
-    end_ingestion_task = DummyOperator(
-        task_id='end_ingestion'
-    )
-
-    # Tasks for audience
-    start_audience_task = DummyOperator(
-        task_id='start_audience'
-    )
-
-    create_audience_run_metadata_task = PythonOperator(
-        task_id='create_audience_run_metadata_table',
-        python_callable=create_audience_run_metadata_table,
-        op_kwargs={'conn_id': 'analytics_redshift'},
-        provide_context=True
-    )
-
-    create_mapping_table_task = PythonOperator(
-        task_id='create_mapping_table',
-        python_callable=create_mapping_table,
-        op_kwargs={'conn_id': 'analytics_redshift'},
-        provide_context=True
-    )
-
-    generate_automatic_audience_task = DummyOperator(
-        task_id='generate_automatic_audience'
-    )
-
-    get_manually_mapped_tables_task = PythonOperator(
-        task_id='get_manually_mapped_tables',
-        python_callable=get_manually_mapped_tables,
-        op_kwargs={'conn_id': 'analytics_redshift'},
-        provide_context=True
-    )
-
-    write_mappings_task = PythonOperator(
-        task_id='write_mappings',
-        python_callable=write_mappings,
-        op_kwargs={'conn_id': 'analytics_redshift'},
-        provide_context=True
-    )
-
-    tag_population_run_task = PythonOperator(
-        task_id='tag_population_run',
-        python_callable=tag_population_run,
-        op_kwargs={'conn_id': 'analytics_redshift'},
-        provide_context=True
+    end_task = DummyOperator(
+        task_id='end'
     )
 
     # DAG for event ingestion
-    start_task >> [create_run_metadata_task,
-                   get_active_experiment_task, generate_run_uuid_task]
+    start_task >> [get_active_experiment_task, generate_start_and_end_ts_task]
     get_active_experiment_task >> generate_missing_event_tables_tasks
-    create_run_metadata_task >> generate_start_and_end_ts_task >> generate_intermediate_events_task
+    generate_start_and_end_ts_task >> generate_intermediate_events_task
     [generate_missing_event_tables_tasks,
         generate_intermediate_events_task] >> populate_experiment_events_task
-    [populate_experiment_events_task,
-        generate_run_uuid_task] >> tag_run_task >> end_ingestion_task >> start_audience_task
-    [populate_experiment_events_task,
-        generate_run_uuid_task] >> cleanup_intermediate_tables_task
-
-    end_ingestion_task >> start_audience_task
-
-    # DAG for the audience ingestion
-    start_audience_task >> [
-        create_audience_run_metadata_task, create_mapping_table_task]
-    create_mapping_table_task >> [
-        generate_automatic_audience_task, get_manually_mapped_tables_task] >> write_mappings_task
-
-    [create_audience_run_metadata_task, write_mappings_task] >> tag_population_run_task
-
-    # DAG for results calculation
+    populate_experiment_events_task >> cleanup_intermediate_tables_task >> end_task
