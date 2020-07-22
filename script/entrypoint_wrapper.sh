a71bc7948611928328bb4ee4d017b4ac3c0cd366
Swapnil Gusani <swapniel99@gmail.com>
2020-07-22 20:37:35 +0530

    rewrite to use official airflow image

---
+++ script/entrypoint_wrapper.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install custom python package if requirements.txt is present
+if [[ -e "/requirements.txt" ]]; then
+    $(command -v pip) install --user -r /requirements.txt
+fi
+
+case "$1" in
+  webserver|worker|flower)
+    # Give the scheduler time to run initdb.
+    sleep 10
+    exec /entrypoint "$@"
+    ;;
+  scheduler)
+    airflow upgradedb
+    exec /entrypoint "$@"
+    ;;
+  bash|python)
+    exec /entrypoint "$@"
+    ;;
+  *)
+    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
+    exec /entrypoint "$@"
+    ;;
+esac
