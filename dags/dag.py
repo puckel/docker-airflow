97217a58f0cef9d397853d8dd5f614acbc82c2ae
EduardoFernandezVillazon <eduardofernandezvillazon4@gmail.com>
2020-09-27 12:47:04 +0100

    functionalities to retrieve rainfall and water level data fully functional, but only work correctly if run from initial state. Successive runs of the dag still show problems

--- dags/dag.py
+++ dags/dag.py
@@ -3,6 +3,8 @@ Code that goes along with the Airflow located at:
 http://airflow.readthedocs.org/en/latest/tutorial.html
 """
 from airflow import DAG
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.operators.dummy_operator import DummyOperator
 from operators.get_stations_api_operator import GetStationsAPIOperator
 from operators.get_hydrology_api_operator import GetHydrologyAPIOperator
 from datetime import datetime, timedelta
@@ -26,7 +28,16 @@ default_args = {
 
 dag = DAG("Hydrology-Data-Project", default_args=default_args) #, schedule_interval=timedelta(1)
 
+PREVIOUS = ExternalTaskSensor(
+    task_id='Previous_Run',
+    external_dag_id="Hydrology-Data-Project",
+    external_task_id='All_Tasks_Completed',
+    allowed_states=['success'],
+    # execution_delta=timedelta(minutes=30),
+    dag=dag
+)
 # t1, t2 and t3 are examples of tasks created by instantiating operators
+
 t1 = GetStationsAPIOperator(
     task_id="Get_Hydrology_Stations_from_API",
     aws_conn_id="aws_credentials",
@@ -55,6 +66,9 @@ t2 = GetHydrologyAPIOperator(task_id="Get_Hydrology_Measures_from_API",
                                  "password": "airflow"
                              },
                              provide_context=True,
+                             observed_property="waterFlow",
+                             columns_to_drop=["measure", "quality"],
+                             general_API_endpoint="https://environment.data.gov.uk/hydrology/data/readings.json?period={period}&station.stationReference={station_reference}&date={date}",
                              date='{{ds}}',
                              dag=dag)
 
@@ -72,6 +86,56 @@ t3 = GetStationsAPIOperator(
     },
     dag=dag)
 
+t4 = GetHydrologyAPIOperator(task_id="Get_Rainfall_Measures_from_API",
+                             source_database={
+                                 "database": "airflow",
+                                 "table": "stations",
+                                 "user": "airflow",
+                                 "password": "airflow"
+                             },
+                             target_database={
+                                 "database": "airflow",
+                                 "table": "measures",
+                                 "user": "airflow",
+                                 "password": "airflow"
+                             },
+                             provide_context=True,
+                             observed_property="rainfall",
+                             general_API_endpoint="https://environment.data.gov.uk/flood-monitoring/id/stations/{station_reference}/readings.json?parameter={observed_property}",
+                             date='{{ds}}',
+                             dag=dag)
+
+t5 = GetHydrologyAPIOperator(task_id="Get_Level_Measures_from_API",
+                             source_database={
+                                 "database": "airflow",
+                                 "table": "stations",
+                                 "user": "airflow",
+                                 "password": "airflow"
+                             },
+                             target_database={
+                                 "database": "airflow",
+                                 "table": "measures",
+                                 "user": "airflow",
+                                 "password": "airflow"
+                             },
+                             provide_context=True,
+                             observed_property="level",
+                             general_API_endpoint="https://environment.data.gov.uk/flood-monitoring/id/stations/{station_reference}/readings.json?parameter={observed_property}",
+                             date='{{ds}}',
+                             dag=dag)
+
+COMPLETE = DummyOperator(
+    task_id='All_Tasks_Completed',
+    dag=dag
+)
+
+t1.set_upstream(PREVIOUS)
 t2.set_upstream(t1)
 t3.set_upstream(t1)
+t4.set_upstream(t3)
+t4.set_upstream(t2)
+t5.set_upstream(t3)
+t5.set_upstream(t2)
+COMPLETE.set_upstream(t4)
+COMPLETE.set_upstream(t5)
 
--- plugins/operators/get_rainfall_api_operator.py
+++ plugins/operators/get_rainfall_api_operator.py
@@ -11,7 +11,7 @@ import boto3
 import datetime
 
 
-class GetHydrologyAPIOperator(BaseOperator):
+class GetRainfallAPIOperator(BaseOperator):
     ui_color = '#358140'
 
     template_fields = ['date']
@@ -23,22 +23,20 @@ class GetHydrologyAPIOperator(BaseOperator):
                  target_database={},
                  origin_sql_connection="",
                  destination_sql_connection="",
-                 observed_property="",
+                 physical_quantity="waterFlow",
                  measures_df=DataFrame([]).empty,
-                 general_API_endpoint="",
-                 columns_to_drop=[],
+                 general_API_endpoint="https://environment.data.gov.uk/hydrology/data/readings.json?period={period}&station.stationReference={station_reference}&date={date}",
                  date="",
                  aws_conn_id='aws_credentials',
                  file_key="",
                  *args, **kwargs):
 
-        super(GetHydrologyAPIOperator, self).__init__(*args, **kwargs)
+        super(GetRainfallAPIOperator, self).__init__(*args, **kwargs)
         self.postgres_conn_id = postgres_conn_id
         self.source_database = source_database
         self.target_database = target_database
-        self.observed_property = observed_property
+        self.physical_quantity = physical_quantity
         self.date = date
-        self.columns_to_drop = columns_to_drop
         self.aws_conn_id = aws_conn_id
         self.file_key = file_key
         self.origin_sql_connection = origin_sql_connection
@@ -49,29 +47,25 @@ class GetHydrologyAPIOperator(BaseOperator):
     def create_connections(self):
         self.origin_sql_connection = create_engine(
             "postgresql+psycopg2://{user}:{password}@postgres:5432/{database}".format(user=self.source_database['user'],
-                                                                                      password=self.source_database[
-                                                                                          'password'],
-                                                                                      database=self.source_database[
-                                                                                          'database']
+                                                                                      password=self.source_database['password'],
+                                                                                      database=self.source_database['database']
                                                                                       )
         )
 
         self.destination_sql_connection = create_engine(
             "postgresql+psycopg2://{user}:{password}@postgres:5432/{database}".format(user=self.target_database['user'],
-                                                                                      password=self.target_database[
-                                                                                          'password'],
-                                                                                      database=self.target_database[
-                                                                                          'database']
+                                                                                      password=self.target_database['password'],
+                                                                                      database=self.target_database['database']
                                                                                       )
         )
 
     def save_locally(self):
         try:
             self.measures_df.to_parquet(
-                fname=self.file_key,
-                partition_cols=["date", "stationReference"],
-                compression='gzip'
-            )
+                                        fname=self.file_key,
+                                        partition_cols=["date", "stationReference"],
+                                        compression='gzip'
+                                        )
         except Exception as e:
             self.log.info(print(e))
             self.log.info(print("Failure to save parquet file locally"))
@@ -98,38 +92,11 @@ class GetHydrologyAPIOperator(BaseOperator):
             self.log.info(print("Failure to save parquet file in s3"))
             raise ValueError
 
-    def date_str_to_dateTime(self):
-        date_time_date = datetime.datetime.strptime(self.date, '%Y-%m-%d')
-        return date_time_date
-
-    def datetime_generator(self):
-        """Generates a string with all the datetime values with which to call the API"""
-        base_string = "dateTime="
-        final_string = ""
-        date_time_date = self.date_str_to_dateTime()
-        time_delta = datetime.timedelta(minutes=15)
-        start_time = datetime.datetime(year=date_time_date.year, month=date_time_date.month, day=date_time_date.day, hour=0, minute=0, second=0, microsecond=0)
-        minute_slots = list(range(0, 96))
-        for i in range(0, 96):
-            minute_slots[i] = minute_slots[i] * time_delta + start_time
-            final_string = final_string + base_string + str(minute_slots[i].date()) + "T" + str(
-                minute_slots[i].time()) + "Z&"
-        return final_string
-
-    def get_api_endpoint(self, station_reference):
-        if self.observed_property == "waterFlow":
+    def read_json(self, station_reference):
+        try:
             API_endpoint = self.general_API_endpoint.format(
                 period=900, station_reference=station_reference, date=self.date
             )
-        else:
-            API_endpoint = self.general_API_endpoint.format(station_reference=station_reference,
-                                                            observed_property=self.observed_property
-                                                            ) + self.datetime_generator()
-        return API_endpoint
-
-    def read_json(self, station_reference):
-        try:
-            API_endpoint = self.get_api_endpoint(station_reference)
             response = requests.get(API_endpoint)
             measures = response.json()["items"]
             self.measures_df = DataFrame.from_dict(measures)
@@ -140,15 +107,12 @@ class GetHydrologyAPIOperator(BaseOperator):
 
     def process_dataframe(self, station_reference, lat, long):
         try:
-            self.log.info(print(self.measures_df.head(0)))
-            if self.observed_property!="waterFlow":
-                self.measures_df["date"] = self.date_str_to_dateTime()
-            else: pass
-            self.measures_df.drop(columns=self.columns_to_drop, inplace=True)
+            columns_to_drop = ["measure"]
+            self.measures_df.drop(columns=columns_to_drop, inplace=True)
             self.measures_df["stationReference"] = station_reference
             self.measures_df["lat"] = lat
             self.measures_df["long"] = long
-            self.measures_df["observedProperty"] = self.observed_property
+            self.measures_df["physicalQuantity"] = self.physical_quantity
             self.measures_df.reset_index(drop=True, inplace=True)
         except Exception as e:
             self.log.info(print(e))
@@ -157,9 +121,8 @@ class GetHydrologyAPIOperator(BaseOperator):
 
     def write_to_local_sql(self):
         try:
-            self.log.info(print(self.measures_df.head(0)))
             self.measures_df.head(0).to_sql(name=self.target_database['table'], con=self.destination_sql_connection,
-                                            if_exists='append', index=False)
+                                       if_exists='append', index=False)
 
             conn = self.destination_sql_connection.raw_connection()
             cur = conn.cursor()
@@ -172,6 +135,19 @@ class GetHydrologyAPIOperator(BaseOperator):
             self.log.info(print(e))
             self.log.info(print("Failure to write to local database"))
             raise ValueError
+
+    def datetime_generator(self):
+        """Generates a string with all the datetime values with which to call the API"""
+        base_string = "dateTime="
+        final_string = ""
+        time_delta = datetime.timedelta(minutes=15)
+        start_time = datetime.datetime(year=2020, month=8, day=28, hour=0, minute=0, second=0, microsecond=0)
+        minute_slots = list(range(0, 96))
+        for i in range(0, 96):
+            minute_slots[i] = minute_slots[i] * time_delta + start_time
+            final_string = final_string + base_string + str(minute_slots[i].date()) + "T" + str(
+                minute_slots[i].time()) + "Z&"
+            print(final_string)
 
     def execute(self, context):
 
@@ -191,7 +167,7 @@ class GetHydrologyAPIOperator(BaseOperator):
             except:
                 self.log.info(print("Station may not have this type of measure"))
                 continue
-            self.file_key = self.target_database['table'] + "/" + self.observed_property + "/" + str(self.date) + "/" +\
+            self.file_key = self.target_database['table'] + "/" + self.physical_quantity + "/" + str(self.date) + "/" +\
                             str(station_reference) + ".parquet.gzip"
 
             self.write_to_local_sql()
