35448f7cc4747efa9bf2235b0929d1b0b64582ea
EduardoFernandezVillazon <eduardofernandezvillazon4@gmail.com>
2020-10-02 16:43:26 +0100

    test version of the automatedLaunch feature

---
+++ script/docker_automated_script.sh
@@ -0,0 +1,6 @@
+echo "PIPPOOOO"
+airflow connections -a --conn_id "aws_credentials" --conn_type "Amazon Web Services" --conn_login "AKIA55OUHUOYUOHCS7W7" --conn_password "6UuGxvoFpAFoeT90lW7StavaO9Mpfy5xTdA2xqg1"
+airflow variables -s "s3_bucket" "uk-hydrology-data-engineering"
+airflow unpause Hydrology-Data-Project
+
+echo "PIPPOOOO"
