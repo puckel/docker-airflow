a26e864efb298e8cb90707c2a881a5f0a3ae6c92
Felix Yuan <felix@classdojo.com>
2020-11-24 17:09:25 -0800

    Update the fast checksum

--- dags/experimental_platform_modules/util.py
+++ dags/experimental_platform_modules/util.py
@@ -1,4 +1,7 @@
+import hashlib
+
+
 def fast_checksum(s):
     # Fast checksum. We don't need it to be too unique, just unique enough to tell us if it's changed
-    checksum = hash(s).to_bytes(8, 'big', signed=True).hex()
+    checksum = hashlib.md5(s.encode('utf-8')).hexdigest()[:16]
     return checksum
