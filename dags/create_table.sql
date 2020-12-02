028fa4d2f81e259e5068d7ac1f2f9bf6997922cf
Maennel <sabine.maennel@liip.ch>
2020-12-02 16:07:13 +0100

    add 3. task: create mysql table

---
+++ dags/create_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE IF NOT EXISTS clean_store_transactions(STORE_ID varchar(50), STORE_LOCATION varchar(50), PRODUCT_CATEGORY varchar(50), PRODUCT_ID int, MRP float, CP float, DISCOUNT float, SP float, DATE date);
--- dags/storedag.py
+++ dags/storedag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.mysql_operator import MySqlOperator
 
 from datacleaner import data_cleaner
 
@@ -17,4 +18,8 @@ dag = DAG('store_dag',default_args=default_args,schedule_interval='@daily', catc
 t1=BashOperator(task_id='check_file_exists', bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv',
                 retries=2, retry_delay=timedelta(seconds=15), dag=dag)
 
-t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
+t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner, dag=dag)
+
+t3 = MySqlOperator(task_id='create_mysql_table', mysql_conn_id="mysql_conn", sql="create_table.sql", dag=dag)
+
+t1 >> t2 >> t3
