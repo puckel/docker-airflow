d1d7c8b7c60d5ff195e3135e1c30a41d28e4c622
Maennel <sabine.maennel@liip.ch>
2020-12-04 17:01:02 +0100

    add task 9 to dag: rename input file by adding a timestamp

--- dags/storedag.py
+++ dags/storedag.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow.operators import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.email_operator import EmailOperator
 
 from datacleaner import data_cleaner
 
@@ -34,4 +35,15 @@ t6 = BashOperator(task_id='move_file1',
 t7 = BashOperator(task_id='move_file2',
                   bash_command='cat ~/store_files_airflow/store_wise_profit.csv && mv ~/store_files_airflow/store_wise_profit.csv ~/store_files_airflow/store_wise_profit_%s.csv' % timestamp, dag=dag)
 
-t1 >> t2 >> t3 >> t4 >> t5 >> [t6,t7]
+t8 = EmailOperator(task_id='send_email',
+                   to='sabine.maennel@gmail.com',
+                   subject='Daily report generated',
+                   html_content=""" <h1>Congratulations! Your store reports are ready.</h1> """,
+                   files=['/usr/local/airflow/store_files_airflow/location_wise_profit_%s.csv' % timestamp,
+                          '/usr/local/airflow/store_files_airflow/store_wise_profit_%s.csv' % timestamp],
+                   dag=dag)
+
+t9 = BashOperator(task_id='rename_raw',
+                  bash_command='mv ~/store_files_airflow/raw_store_transactions.csv ~/store_files_airflow/raw_store_transactions_%s.csv' % timestamp, dag=dag)
+
+t1 >> t2 >> t3 >> t4 >> t5 >> [t6, t7] >> t8 >> t9
--- mysql.md
+++ mysql.md
@@ -7,8 +7,17 @@ use mysql;
 show tables;
 ```
 
-Create connection
+## Create connection
 
 - see dag for connection name:  `mysql_conn_id="mysql_conn"` 
 - create new connection `mysql_conn`
 - connection type `MySQL, `host `mysql`, schema `mysql`, login `root`, password
+
+## Troubleshooting
+
+```
+cryptography.fernet.InvalidToken
+```
+
+- check the connection and repair it if needed.
+
